4f937f055a05c830e018874782f12072643f11ad
YOGEESH K P <yogeesh793@gmail.com>
2025-02-08 17:13:48 +0530

    Add files via upload

---
+++ 1pandas.py
@@ -0,0 +1,17 @@
+import pandas as pd
+mydatabase = {
+    "cars": ["Toyota", "Honda", "TATA"],
+    "Passangers": [4, 5, 6]
+}
+myver = pd.DataFrame(mydatabase)
+print(myver)
+
+print("-------------------------------------")
+
+import pandas as pb
+mydb = {
+    "Name":["yogi","hunuman"],
+    "score":[100,100]
+}
+mye=pb.DataFrame(mydb)
+print(mye)
